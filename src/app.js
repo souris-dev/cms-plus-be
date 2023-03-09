0228e2a6350e57128e53312b228ba4a594c5694c
Souris Ash <souris_ash@mckinsey.com>
2023-03-10 04:12:50 +0530

    feat: mount cms route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cmsRoute from './routes/route.js';
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 8002;
 
-app.use('/', () => {});
+app.use('/api', cmsRoute);
 
 app.listen(port, () => {
   console.log(`cms-service listening on port ${port}`);
