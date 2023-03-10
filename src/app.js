f53613bee279bda97e22df1ae9016e732103679d
Souris Ash <souris_ash@mckinsey.com>
2023-03-10 20:23:42 +0530

    fix: add cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import cmsRoute from './routes/cmsRoute.js';
 import healthCheckRoute from './routes/healthCheckRoute.js';
-//import { checkToken } from './utils/middleware/auth.js';
+import cors from 'cors';
+import { checkToken } from './utils/middleware/auth.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 8003;
 
 app.use('/', healthCheckRoute);
-app.use('/api', cmsRoute);
+
+if (process.env.USE_AUTH) {
+  app.use('/api', checkToken, cmsRoute);
+}
+else {
+  app.use('/api', cmsRoute);
+}
 
 app.listen(port, () => {
   console.log(`cms-service listening on port ${port}`);
